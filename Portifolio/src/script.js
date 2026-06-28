1aad352666e68ab21d22eb870d8623fa695f9c4f
lanser-dos-programa <luizlanser12@gmail.com>
2026-06-28 18:48:13 -0300

    Refactor techs list and improve code readability
    Clone techs list for repeated display and clean up code.

--- Portifolio/src/script.js
+++ Portifolio/src/script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  lucide.createIcons();
+
   const canvas = document.getElementById("snow");
   const ctx = canvas.getContext("2d");
-
   let width = window.innerWidth;
   let height = window.innerHeight;
-
   canvas.width = width;
   canvas.height = height;
-
   canvas.style.position = "fixed";
   canvas.style.top = "0";
   canvas.style.left = "0";
@@ -21,7 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
     constructor() {
       this.reset();
     }
-
     reset() {
       this.x = Math.random() * width;
       this.y = Math.random() * -height;
@@ -29,16 +28,11 @@ document.addEventListener("DOMContentLoaded", () => {
       this.speed = Math.random() * 0.3 + 0.15;
       this.wind = Math.random() * 0.2 - 0.1;
     }
-
     update() {
       this.y += this.speed;
       this.x += this.wind;
-
-      if (this.y > height) {
-        this.reset();
-      }
+      if (this.y > height) this.reset();
     }
-
     draw() {
       ctx.beginPath();
       ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
@@ -74,9 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
   initSnow();
   animateSnow();
 
-
   const indicator = document.querySelector('.scroll-indicator');
-
   window.addEventListener('scroll', () => {
     if (window.scrollY > 20) {
       indicator.classList.add('hide');
@@ -85,6 +77,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  const techsList = document.querySelector('.techs-list');
+  const techsClone = techsList.cloneNode(true);
+  techsList.parentNode.appendChild(techsClone);
 
   const links = {
     "Next.js": "https://nextjs.org",
@@ -97,7 +92,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "Git": "https://git-scm.com",
     "PostgreSQL": "https://www.postgresql.org",
     "Alchemy": "https://www.sqlalchemy.org",
-    "SQlite": 'https://sqlitebrowser.org/',
+    "SQlite": "https://sqlitebrowser.org/",
     "JavaScript": "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
     "C#": "https://learn.microsoft.com/pt-br/dotnet/csharp/",
     "Postman": "https://www.postman.com",
@@ -108,10 +103,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelectorAll(".tech-item").forEach(item => {
     const name = item.querySelector("span")?.innerText.trim();
-
     if (links[name]) {
       item.style.cursor = "pointer";
-
       item.addEventListener("click", () => {
         window.open(links[name], "_blank", "noopener");
       });
